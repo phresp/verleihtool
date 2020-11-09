c9286204a7195a9a0a787d07e4c705660318a53b
Philipp Spanner <philipp.spanner@googlemail.com>
2020-11-09 08:38:38 +0100

    Testuserinput keys_production for heroku deploy test

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,6 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/verleihtool",
+  //mongoURI: "mongodb://localhost:27017/verleihtool",
+  mongoURI:
+    "mongodb+srv://phresp:WQcBDmb9qCjSSj1g@leihtool.wtp9b.mongodb.net/verleihtool?retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,10 @@
+// module.exports = {
+//   mongoURI: process.env.MONGO_URI,
+//   secretOrKey: process.env.SECRET_OR_KEY,
+// };
+
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY,
+  mongoURI:
+    "mongodb+srv://testuser:Zf3HD2FbHIe0TnKG@leihtool.wtp9b.mongodb.net/leihtool?retryWrites=true&w=majority",
+  secretOrKey: "secret1234567890",
 };
