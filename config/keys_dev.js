45ede1938b113806aa977352b20f1f7311d18a01
Philipp Spanner <philipp.spanner@googlemail.com>
2020-11-09 08:51:21 +0100

    removed the exposed keys

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,6 +1,4 @@
 module.exports = {
-  //mongoURI: "mongodb://localhost:27017/verleihtool",
-  mongoURI:
-    "mongodb+srv://phresp:WQcBDmb9qCjSSj1g@leihtool.wtp9b.mongodb.net/verleihtool?retryWrites=true&w=majority",
+  mongoURI: "mongodb://localhost:27017/verleihtool",
   secretOrKey: "secret",
 };
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,10 +1,4 @@
-// module.exports = {
-//   mongoURI: process.env.MONGO_URI,
-//   secretOrKey: process.env.SECRET_OR_KEY,
-// };
-
 module.exports = {
-  mongoURI:
-    "mongodb+srv://testuser:Zf3HD2FbHIe0TnKG@leihtool.wtp9b.mongodb.net/leihtool?retryWrites=true&w=majority",
-  secretOrKey: "secret1234567890",
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
 };
