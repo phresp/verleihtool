1572965ee2630b47d6f9447dc5bde5e0549ff93f
Philipp Spanner <philipp.spanner@googlemail.com>
2020-11-19 11:34:15 +0100

    Expires in changed

--- routes/api/users.js
+++ routes/api/users.js
@@ -93,7 +93,7 @@ router.post("/login", (req, res) => {
         jwt.sign(
           payload,
           keys.secretOrKey,
-          { expiresIn: 3600 },
+          { expiresIn: "8h" },
           (err, token) => {
             res.json({
               success: true,
